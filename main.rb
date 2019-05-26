a69602b1c4f5348d68f412515870b64497b534c0
Paulo Jansen <pauloo.jansen@gmail.com>
2019-05-26 13:40:21 -0300

    adding necessities

--- main.rb
+++ main.rb
@@ -1,25 +1,74 @@
 
+class Necessity
+
+  attr_reader :name, :phase
+
+  def initialize(time, name)
+    @phase = 0
+    @time = time
+    @cooldown = time
+    @name = name
+  end
+
+  def cooldown
+    @cooldown -= 1
+    if @cooldown == 0
+      up
+      @cooldown = @time
+    end
+  end
+
+  def up
+    @phase += 1 if @phase < 4
+
+  end
+
+  def down
+    @phase -= 1 if @phase > 0
+  end
+
+end
+
+
+class Pet
+
+  attr_reader :name, :necessities
+
+  def initialize(pet_name)
+    @name = pet_name
+    @necessities = [
+      Necessity.new(30, "Hunger"),
+      Necessity.new(10, "Fun"),
+      Necessity.new(50, "Hygiene")
+    ]
+  end
+
+end
+
 
 class Game
 
   def start
     puts "Type pet name:"
-    @pet_name = gets
+    @pet = Pet.new(gets)
     gameloop
   end
 
   def update
-
+    @pet.necessities.each do |n|
+      n.cooldown
+    end
   end
 
   def draw
-    puts "Pet: #{@pet_name}"
-    puts '###############################'
-    puts '#     ####### #################'
-    puts '####  ######  #################'
-    puts '#### ######  ##################'
-    puts '#####       ###################'
-    puts '###############################'
+    puts "Pet: #{@pet.name}"
+
+    necessities_line = ""
+    @pet.necessities.each do |n|
+      necessities_line += "#{n.name}: #{n.phase}  "
+    end
+    puts necessities_line
+
   end
 
   def gameloop
